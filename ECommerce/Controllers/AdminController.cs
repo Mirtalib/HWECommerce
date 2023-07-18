a9d91add829df69d2150cc3f6c1d5127d98d519e
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2023-07-19 01:14:42 +0400

    Add project files.

---
+++ ECommerce/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using ECommerce.Data;
+using ECommerce.Helpers;
+using ECommerce.Models;
+using ECommerce.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ECommerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public AdminController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(context.Products.ToList());
+        }
+
+        public IActionResult AddProduct()
+        {
+            ViewBag.Categories = new SelectList(context.Categories, "Id", "Name");
+            ViewBag.Tags = new MultiSelectList(context.Tags, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(AddProductViewModel model)
+        {
+            try
+            {
+                string path = await UploadFileHelper.UploadFile(model.ImageUrl);
+
+                Product product = new()
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    ImageUrl = path,
+                    CategoryId = model.CategoryId,
+                };
+
+                context.Add(product);
+                await context.SaveChangesAsync();
+
+                foreach (var tag in model.TagIds)
+                {
+                    context.Add(new ProductTag { TagId = tag, ProductId = product.Id });
+                }
+
+                await context.SaveChangesAsync();
+
+                return View("Index.cshtml");
+            }
+            catch (Exception)
+            {
+
+                return View("error.cshtml");
+            }
+        }
+    }
+}
---
+++ ECommerce/Models/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Models.ViewModels
+{
+    public class AddProductViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public int[] TagIds { get; set; }
+    }
+}
